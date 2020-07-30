cd6ffddb5bba972becdad8ca6afa425043a25806
Jorge Meza <jorge.meza.rdz@gmail.com>
2020-07-30 13:39:12 -0500

    first commit

--- ky038.py
+++ ky038.py
@@ -1,16 +1,18 @@
 import RPi.GPIO as GPIO
 import time
 
-GPIO.setmode(GPIO.BOARD)
+GPIO.setmode(GPIO.BCM)
 GPIO.setup(23, GPIO.IN)
 GPIO.setup(4, GPIO.OUT)
 GPIO.setup(17, GPIO.OUT)
 flag=False
 delay=0.05
+print("inicio")
 while True:
-   if GPIO.input(3):
+   if GPIO.input(4):
        time.sleep(180) 
        flag=not flag
+       print(flag)
        GPIO.output(4,flag)
        GPIO.output(17,flag)
 
